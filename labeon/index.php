fbf734a96ad674a2f32daf1e75efdbd19dad098e
maicon <maicon@cleoapp.com.br>
2019-11-13 20:59:31 -0200

    Site
    Site desenvolvido para o labeon, laboratório localizado na Universidade Federal de Santa Maria -RS. No ano de 2015.

---
+++ labeon/index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+<title>NuPEAD</title>
+<meta name="description" content="">
+<meta name="keywords" content="UFSM Lablin">
+
+<!-- Mobile viewport -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+
+<link rel="shortcut icon" href="images/ufsm.ico"  type="image/x-icon">
+
+<!-- CSS-->
+<!-- Google web fonts. You can get your own bundle at http://www.google.com/fonts. Don't forget to update the CSS accordingly!-->
+<link href='http://fonts.googleapis.com/css?family=Droid+Serif|Ubuntu' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Pavanam" rel="stylesheet" type='text/css'>
+
+<link rel="stylesheet" href="css/normalize.css">
+<link rel="stylesheet" href="js/flexslider/flexslider.css">
+<link rel="stylesheet" href="css/basic-style.css">
+<!-- end CSS-->
+    
+<!-- JS-->
+<script src="js/libs/modernizr-2.6.2.min.js"></script>
+<!-- end JS-->
+
+
+</head>
+
+<body id="home">
+    
+    <!--
+    
+    <div class="top">
+        <div class="wrapper clearfix">
+            <a href="http://site.ufsm.br/" target="_blank"><img src="images/ufsm.png" alt=""></a>
+        </div>
+        <div class="tophr"></div>
+    </div>
+    
+    -->
+    
+<!-- header area -->
+    <header class="wrapper clearfix">
+		       
+        <div id="banner">
+                
+        	<div id="logo">
+                    <a href="index.php">
+                        <img src="images/nupead.png" alt="logo"></a>
+                </div>
+        </div>
+        
+        <!-- main navigation -->
+        <nav id="topnav">
+        <div class="menu-toggle">Menu</div>  
+            <ul class="srt-menu" id="menu-main-navigation">
+		<li class="current"><a href="#">Home</a></li>
+                <li><a href="#">Produções</a>
+                        <ul>
+                            <li><a href="pag/projetos.php">Projetos</a></li>
+                            <li><a href="pag/artigos.php">Artigos</a></li>
+                            <li><a href="pag/dissertacao.php">Dissertações</a></li>
+                            <li><a href="pag/pesquisa.php">Pesquisa</a></li>
+                        </ul>
+                </li>
+                <li><a href="#">Lablin</a>
+                        <ul>
+                            <li><a href="http://coral.ufsm.br/lablin/" target="_blank">Site Lablin</a></li>
+                            <li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdOxPHQ4pEwYTmr7VvxE_wHe1uvfBXIRWrywzAFMFJ8iUyEYA/viewform" target="_blank">Agendamento de Salas</a></li>
+                        </ul>
+                </li>
+                <li><a href="#">LabEon</a>
+                        <ul>
+                                <li><a href="http://ufsmlablin.com/" target="_blank">Cursos Online</a></li>
+                                <li><a href="pag/material.php">Material Digital</a></li>
+                                <li><a href="#">Equipe</a>
+                                    <ul>
+                                        <li><a href="pag/professor.php">Professor</a></li>
+                                        <li><a href="pag/mest.php">Mestrandos</a></li>
+                                        <li><a href="pag/iniciacao_c.php">Iniciação Cientifica e Bolsistas</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                </li>
+                <li><a href="http://www.englishonline3d.com" target="_blank">E-3D</a></li>
+                <li><a href="pag/eventos.php">Eventos</a></li>
+                <li><a href="pag/jogo.php">Jogo</a></li>
+                <li><a href="pag/contato.php">Contato</a></li>
+            </ul>     
+        </nav><!-- #topnav -->
+            
+    </header><!-- end header -->
+ 
+ 
+<!-- hero area (the grey one with a slider -->
+    <section id="hero" class="clearfix">    
+    <!-- responsive FlexSlider image slideshow -->
+    <div class="wrapper">
+       <div class="row"> 
+        <div class="grid_6">
+            <h2>NuPEAD</h2>
+            <p class="paragraph">   O Núcleo de Pesquisa, Ensino e Aprendizagem de Línguas a Distância tem como objetivo geral realizar investigações dentro 
+                                    das linhas de pesquisas que abordam os processos de Ensino e Aprendizagem de Línguas na modalidade a distância, tais como 
+                                    o Letramento digital, a formação de professores para EAD, a Produção de material didático digital para ensino a distância 
+                                    e presencial e o desenvolvimento de jogos educativos digitais. </p>
+            <p class="paragraph">   Atualmente, estão em desenvolvimento diferentes projetos que investigam os processos de ensino e aprendizagem realizados 
+                                    por meio de ambientes virtuais, focalizando na investigação das práticas sociais e discursivas realizadas no contexto digital. 
+                                    No núcleo de pesquisa é oportunizado aos alunos de letras, e de outras áreas , a produção de material didático digital e 
+                                    de jogos educacionais tanto como material complementar para sala de aula quanto para o ensino a distância.</p>
+			
+            <p><a href="pag/contato.html" class="buttonlink">Entre em contato</a> <a href="http://dgp.cnpq.br/dgp/espelhogrupo/9244673102341576" target="_blank" class="buttonlink">Link do Cnpq</a></p>
+        
+		</div>
+        <div class="grid_6 rightfloat">
+            <br>
+              <div class="flexslider">
+                  <ul class="slides">
+                      <li>
+                          <img src="images/noticia1.jpg" alt="">
+                          <p class="flex-caption">II Sigatec</p>
+                      </li>
+                      <li>
+                          <a><img src="images/foto2.jpg"  alt=""></a>
+                          <p class="flex-caption">Legenda foto 2</p>
+                      </li>
+                      <li>
+                          <img src="images/foto3.jpg" alt="">
+                          <p class="flex-caption">Legenda foto 3</p>
+                      </li>
+                      <li>
+                          <img src="images/foto4.jpg" alt="">
+                          <p class="flex-caption">Legenda foto 4</p>
+                      </li>
+                  </ul>
+                </div><!-- FlexSlider -->
+              </div><!-- end grid_7 -->
+        </div><!-- end row -->
+       </div><!-- end wrapper -->
+    </section><!-- end hero area -->
+
+
+<!-- main content area -->   
+<div id="main" class="wrapper">
+    
+    
+<!-- content area -->    
+	<section id="content" class="wide-content">
+      <div class="row">	
+        <div class="grid_4">
+        	<h2 class="first-header">Realizado o II SIGATEC</h2>
+            <img src="images/noticia1.jpg" alt="">
+            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+			<p><a href="#" class="buttonlink">Leia Mais</a></p>
+		</div>
+        
+        <div class="grid_4">
+        	<h2 class="first-header">Alunos apresentam na JAI</h2>
+            <img src="images/noticia2.jpg" alt="">
+            <p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. m</p>
+			<p><a href="#" class="buttonlink">Leia Mais</a></p>
+		</div>
+        
+        <div class="grid_4">
+        	<h2 class="first-header">Cursos de Língua Inglesa</h2>
+            <img src="images/foto3.jpg" alt="">
+            <p class="paragraph">O Laboratório de Ensino e Aprendizagem de Línguas Online (LabEOn) e o Laboratório de Línguas (Lablin) estão ofertando cursos de língua inglesa em nível básico (nível 1), na modalidade a distância (EaD).Os alunos poderão realizar suas inscrições de 2 a 15 de março, na sala 3311, no Centro de Educação (prédio 16).
+</p>
+			<p><a href="noticias/curso-linguas.php" class="buttonlink">Leia Mais</a></p>
+		</div>
+	  </div><!-- end row -->
+	</section><!-- end content area -->
+    
+      
+  </div><!-- #end div #main .wrapper -->
+
+
+
+<!-- footer area -->    
+<footer>
+	<div id="colophon" class="wrapper clearfix">
+            <div class="grid_6">
+				
+			<h3>Universidade Federal de Santa Maria - RS</h3>
+		<br>
+                        <div>
+                            <p> Av. Roraima nº 1000<br>
+                                Cidade Universitária<br>
+                                Bairro Camobi<br>
+                                Santa Maria &nbsp; RS<br>
+                                CEP: 97105-900<br>
+                                +55 (55) 3220-8000</p>
+			</div>
+		
+            </div>
+            
+            <div class="grid_6">
+				
+				<h3>Lablin (Laboratório de Línguas)</h3>
+		<br>
+                        <div>
+                            <p> Av. Roraima nº 1000<br>
+                                Prédio 16 &#8208; Sala 3311<br>
+                                Bairro Camobi<br>
+                                Santa Maria RS <br>
+                                CEP: 97105-970<br>
+                                +55 (55) 3220-8410<br>
+                                lablinadm@gmail.com</p>
+			</div>
+            </div>
+            <div class="grid_12">
+                <hr>
+                <p>
+                    Copyright &copy; 2016. Lablin
+                    <a class="fly_right" href="https://www.facebook.com/Nupead-995012877215468/" target="_blank"><img src="images/branco/nupead's logo.png" width="60" title="NuPEAD" alt="NuPEAD"></a>
+                    <a class="fly_right" href="https://www.facebook.com/Lablin-558434187547931/" target="_blank"><img src="images/branco/LABLIN.png" width="60" title="Lablin" alt="Lablin"></a>
+                    <a class="fly_right" href="https://www.facebook.com/labeon.ufsm/" target="_blank"><img src="images/branco/LAB EON3.png" width="60" title="LabEon" alt="LabEon"></a>
+                    <a class="fly_right" href="http://englishonline3d.com/" target="_blank"><img src="images/branco/e-3d-branco.png" width="50" title="E-3D" alt="E-3D"></a>
+                    <a class="fly_right" href="http://ufsmlablin.com/" target="_blank"><img src="images/branco/moodle-logo-white.png" width="50" title="Moodle" alt="Moodle"></a>
+                </p>
+            </div>
+
+        </div>
+    
+</footer><!-- #end footer area --> 
+
+
+<!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.9.0.min.js">\x3C/script>')</script>
+
+<script defer src="js/flexslider/jquery.flexslider-min.js"></script>
+
+<!-- fire ups - read this file!  -->   
+<script src="js/main.js"></script>
+
+</body>
+</html>
